17f66af273ba14ee6f60a7fc0c6cab66730a8950
sadat <shameemsadat00@gmail.com>
2025-08-26 04:27:14 +0600

    jsx, working

--- client/vite.config.js
+++ client/vite.config.js
@@ -1,7 +1,7 @@
+// https://vitejs.dev/config/
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
   optimizeDeps: {
